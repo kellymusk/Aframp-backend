ef121fd7d5a48aa2f4ce061856fca4c20284c4c7
Ajidokwu Sabo <realjaiboi70@gmail.com>
2026-01-27 11:01:02 +0100

    feat: Afri scafold

--- contract/contracts/stellar/src/lib.rs
+++ contract/contracts/stellar/src/lib.rs
@@ -1,23 +1,34 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
+
+mod contract;
+mod storage;
+mod events;
+mod error;
+
+use soroban_sdk::{contract, contractimpl, Env, Address};
 
 #[contract]
-pub struct Contract;
+pub struct AfrIContract;
 
-// This is a sample contract. Replace this placeholder with your own contract logic.
-// A corresponding test example is available in `test.rs`.
-//
-// For comprehensive examples, visit <https://github.com/stellar/soroban-examples>.
-// The repository includes use cases for the Stellar ecosystem, such as data storage on
-// the blockchain, token swaps, liquidity pools, and more.
-//
-// Refer to the official documentation:
-// <https://developers.stellar.org/docs/build/smart-contracts/overview>.
 #[contractimpl]
-impl Contract {
-    pub fn hello(env: Env, to: String) -> Vec<String> {
-        vec![&env, String::from_str(&env, "Hello"), to]
+impl AfrIContract {
+    pub fn init(env: Env, admin: Address) {
+        storage::set_admin(&env, &admin);
+    }
+
+    pub fn mint(env: Env, to: Address, amount: i128) {
+        contract::mint(env, to, amount);
+    }
+
+    pub fn burn(env: Env, from: Address, amount: i128) {
+        contract::burn(env, from, amount);
+    }
+
+    pub fn transfer(env: Env, from: Address, to: Address, amount: i128) {
+        contract::transfer(env, from, to, amount);
+    }
+
+    pub fn balance(env: Env, user: Address) -> i128 {
+        storage::get_balance(&env, &user)
     }
 }
-
-mod test;
