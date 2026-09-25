3390f78280e22d23d6ebf1197d0efc87b16acafa
djcdz4 <djcdz4@users.noreply.github.com>
2026-09-25 23:25:26 +0000

    fix: #1073 Bug: missing `updated_at` column in merchants table - GET /me
    Closes #1073

--- src/models/merchant.rs
+++ src/models/merchant.rs
@@ -1,18 +1,12 @@
 use chrono::{DateTime, Utc};
 use serde::Serialize;
-use sqlx::FromRow;
 use uuid::Uuid;
 
-#[derive(Debug, Clone, Serialize, FromRow)]
+#[derive(Debug, Clone, Serialize, sqlx::FromRow)]
 pub struct Merchant {
     pub id: Uuid,
     pub user_id: Uuid,
     pub name: String,
     pub created_at: DateTime<Utc>,
-}
-
-#[derive(Debug, Clone)]
-pub struct NewMerchant {
-    pub user_id: Uuid,
-    pub name: String,
+    pub updated_at: DateTime<Utc>,
 }
